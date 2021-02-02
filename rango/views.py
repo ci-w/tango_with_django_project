56002cd09b1094616e6a8a1cbacace63ab9408ce
Claire Williamson <2464406w@student.gla.ac.uk>
2021-02-02 17:56:54 +0000

    end of chapter 4

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    #Construct a dict to pass to the template engine
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    #Returns a rendered response to send to the client
+    return render(request, 'rango/index.html', context=context_dict)
+   
+
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by Claire.'}
+    return render(request, 'rango/about.html', context=context_dict)
 
